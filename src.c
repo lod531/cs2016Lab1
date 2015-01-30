61f71eb8ef296aca46b08079be5412738bf8df7c
lod531 <buinovsa@tcd.ie>
2015-01-30 15:31:12 +0000

    Create src.c
    Core source file

---
+++ src.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+const int NUMBER_OF_THREADS = 4;
+
+void *PrintHello(void *threadid) 
+{
+	printf("\n%d: Hello World!\n", threadid);
+	pthread_exit(NULL);
+}
+
+int main (int argc, const char * argv[]) 
+{
+	pthread_t threads[NUM_THREADS];
+	int rc,t;
+	for (t=0;t<NUM_THREADS;t++) 
+	{
+		printf("Creating thread %d\n",t);
+		rc = pthread_create(&threads[t],NULL, PrintHello,(void *)t);
+		if (rc) 
+		{
+			printf("ERROR return code from pthread_create(): %d\n",rc);
+			exit(-1);
+		}
+	}
+
+	for(t=0;t<NUM_THREADS;t++) 
+	{
+		 pthread_join( threads[t], NULL);
+	}
+}
