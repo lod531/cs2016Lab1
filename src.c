bc9cff7109811f910b6bc0b8252d9daf0bbcbba0
lod531 <buinovsa@tcd.ie>
2015-01-30 16:36:42 +0000

    Update src.c

--- src.c
+++ src.c
@@ -3,22 +3,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-const int NUMBER_OF_THREADS = 4;
+const int NUMBER_OF_PI_DIGITS = 4;
 
-void *PrintHello(void *threadid) 
+int exponent(int base, int exponent)
 {
-	printf("\n%d: Hello World!\n", threadid);
+	int result = 1;
+	int i;
+	for(i = 0; i < exponent; i++)
+	{
+		result *= base;
+	}
+	return result;
+}
+
+double BBP(int k)
+{
+	int i;
+	double result = 0;
+	for(i = 0; i < k; i++)
+	{	
+		double currentTerm = 1;
+
+		currentTerm /= exponent(16, i);
+
+		currentTerm *= 4*(8*i + 1) -2*(8*i + 4) -1*(8*i + 5) -1*(8*i + 6);
+
+		result += currentTerm;
+	}
+	return result;
+}
+
+void * calculatePiDigit(void *threadid) 
+{
+	int temp = (int)threadid + 1;
+	int i;
+	int digit = 0;
+	for(i = 0; i < temp; i++)
+	{
+		int quotient = exponent(16, i);
+
+	}
 	pthread_exit(NULL);
 }
 
 int main (int argc, const char * argv[]) 
 {
-	pthread_t threads[NUM_THREADS];
+	double test = BBP(1);
+	printf("%lf\n",test);
+	/*
+	pthread_t threads[NUMBER_OF_PI_DIGITS];
 	int rc,t;
-	for (t=0;t<NUM_THREADS;t++) 
+	for (t=0;t<NUMBER_OF_PI_DIGITS;t++) 
 	{
 		printf("Creating thread %d\n",t);
-		rc = pthread_create(&threads[t],NULL, PrintHello,(void *)t);
+		rc = pthread_create(&threads[t],NULL,  calculatePiDigit,(void *)t);
 		if (rc) 
 		{
 			printf("ERROR return code from pthread_create(): %d\n",rc);
@@ -26,8 +64,9 @@ int main (int argc, const char * argv[])
 		}
 	}
 
-	for(t=0;t<NUM_THREADS;t++) 
+	for(t=0;t<NUMBER_OF_PI_DIGITS;t++) 
 	{
 		 pthread_join( threads[t], NULL);
 	}
+	*/
 }
